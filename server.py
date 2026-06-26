028dfa2b38b88c5de21b3a64559ff17759e45002
JohnPC <johnjohnjohnkimchi@gmail.com>
2026-06-26 11:00:34 -0400

    Established successfull connection and commands to govee devices

--- server.py
+++ server.py
@@ -1,15 +1,28 @@
 from socket import *
 import json
 
-UDP_IP = "239.255.255.250"
-SEND_PORT = 4001
-LISTEN_PORT = 4002
-CONTROL_PORT = 4003
+MCAST_GRP = "239.255.255.250"
+SEND_PORT = 4001     # devices listen here for scan requests
+LISTEN_PORT = 4002   # devices reply to the multicast group on this port
+CONTROL_PORT = 4003  # send control commands to a device's IP here
 
-sendSocket = socket(AF_INET, SOCK_DGRAM)
+# The local IP of the interface on the SAME network as your Govee device.
+# Set this explicitly on Windows so multicast doesn't leave via a VPN/WSL/Hyper-V
+# adapter. Find it with `ipconfig` (the IPv4 address of your Wi-Fi/Ethernet).
+LOCAL_IP = "192.168.2.65"  # e.g. "192.168.1.50"
+
+# --- Listen socket: must JOIN the multicast group to hear device replies ---
 listenSocket = socket(AF_INET, SOCK_DGRAM)
+listenSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 listenSocket.bind(("", LISTEN_PORT))
-listenSocket.settimeout(240)
+mreq = inet_aton(MCAST_GRP) + inet_aton(LOCAL_IP)
+listenSocket.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+listenSocket.settimeout(10)
+
+# --- Send socket: pin the outgoing interface for multicast ---
+sendSocket = socket(AF_INET, SOCK_DGRAM)
+sendSocket.setsockopt(IPPROTO_IP, IP_MULTICAST_IF, inet_aton(LOCAL_IP))
+sendSocket.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 2)
 
 scan_msg = json.dumps({
     "msg": {
@@ -18,7 +31,7 @@ scan_msg = json.dumps({
     }
 }).encode()
 
-sendSocket.sendto(scan_msg, (UDP_IP, SEND_PORT))
+sendSocket.sendto(scan_msg, (MCAST_GRP, SEND_PORT))
 
 devices = []
 
@@ -31,3 +44,19 @@ while True:
     except timeout:
         print(f"Done scanning. Found {len(devices)} device(s).")
         break
+
+# print(devices)
+# Testing just for the one device connected
+
+#command to turn off light (0 - off, 1 - on)
+command = json.dumps({
+    "msg":{
+        "cmd" : "turn",
+        "data":{
+            "value" : 0
+        }
+    }
+})
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.sendto(command.encode(), (devices[0]["ip"], CONTROL_PORT))
